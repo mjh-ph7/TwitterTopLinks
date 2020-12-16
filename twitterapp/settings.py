0c754032edc072d7c9563843baa79ed176f3912c
mjh <mjh5376@gmail.com>
2020-12-17 01:25:23 +0530

    first commit

--- twitterapp/settings.py
+++ twitterapp/settings.py
@@ -18,7 +18,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'twitterapp/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
