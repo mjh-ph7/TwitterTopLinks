7555e1125a537005f8681b9e3164c98cec06e67a
mjh <mjh5376@gmail.com>
2020-12-18 01:52:51 +0530

    heroku fix

--- twitterapp/settings.py
+++ twitterapp/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'srsesh7o1-a5u6g9o79rw(3qkd!7gb1zb(+m3@82gurfb5d5+1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['twitter-tl.herokuapp.com']
 
